76890c605b942e480470df9eafcebf84f755bca4
maryserar8903 <mary.serna.8903@gmail.com>
2019-12-11 04:16:27 +0000

    Uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for j in str:
+        if ord(j) >= 97 and ord(j) <= 123:
+            j = (ord(j) - 32)
+        else:
+            j = ord(j)
+        print("{:c}".format(j), end="")
+    print("")
