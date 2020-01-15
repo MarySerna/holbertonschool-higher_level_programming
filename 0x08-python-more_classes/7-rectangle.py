5bd671bd9c292822417232b2c25a02dbf3f673be
maryserar8903 <mary.serna.8903@gmail.com>
2020-01-15 17:54:45 +0000

    Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -68,7 +68,7 @@ class Rectangle:
     def __str__(self):
         """Prints the rectangle with the character #
         """
-        rect= ""
+        rect = ""
         if self.width == 0 or self.__height == 0:
             return ""
         for i in range(self.__height):
