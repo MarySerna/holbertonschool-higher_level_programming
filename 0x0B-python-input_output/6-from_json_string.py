1968b0d9a663ced4bc2e01c5583ff23272a02042
maryserar8903 <mary.serna.8903@gmail.com>
2020-01-22 01:27:45 +0000

    From JSON string to Object

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+import json
+
+
+"""
+function that returns an object
+"""
+
+
+def from_json_string(my_str):
+    """
+    return a object
+    """
+
+    return json.loads(my_str)
