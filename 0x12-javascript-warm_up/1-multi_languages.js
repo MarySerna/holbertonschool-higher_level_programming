d746cf9cc63aa829d98ca35a7e425f5b451a84ee
MarySerna <mary.serna.8903@gmail.com>
2020-05-05 21:54:22 +0000

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,6 @@
 /*
 Script that prints 3 lines
 */
-console.log("C is fun");
-console.log("Python is cool");
-console.log("Javascript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
