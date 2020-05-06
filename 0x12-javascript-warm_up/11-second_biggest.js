630693675ccb33a07e2d06cb51cc5664604bc5dd
MarySerna <mary.serna.8903@gmail.com>
2020-05-06 03:55:56 +0000

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,9 @@ Write a script that searches the second biggest integer in the list of arguments
 let nextMax = 0;
 const args = process.argv.slice(2);
 if (args.length > 1) {
-  args.sort();
+  args.sort(function (a, b) {
+    return a - b;
+  });
   nextMax = args[args.length - 2];
 }
 console.log(nextMax);
