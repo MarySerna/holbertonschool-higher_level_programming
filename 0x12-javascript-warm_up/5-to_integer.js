9eef3325a1d0e64dedfc1596940eae7f51bd585e
MarySerna <mary.serna.8903@gmail.com>
2020-05-06 01:14:34 +0000

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 /*
 Script that prints My number: <first argument converted in integer>
-if the first argument can be converted to an intege
+if the first argument can be converted to an integer
 */
 const arg = process.argv[2];
 const r = parseInt(arg);
 if (isNaN(r)) {
-  console.log('Not a number');
+  console.log('Not a number: ');
 } else {
-  console.log('My number is: ' + r);
+  console.log('My number ' + r);
 }
