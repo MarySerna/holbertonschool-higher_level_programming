38002a4ceef26e5ec13378c4761a04ad6d0fb868
MarySerna <mary.serna.8903@gmail.com>
2020-05-06 02:51:57 +0000

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+Write a script that prints a square
+*/
+const x = process.argv[2];
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
+}
