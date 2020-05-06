80b73954b3c13db2045f7c9dc8a9ba9c45ebafe2
MarySerna <mary.serna.8903@gmail.com>
2020-05-06 00:20:19 +0000

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,9 +3,9 @@
 Script that prints My number: <first argument converted in integer>
 if the first argument can be converted to an intege
 */
-const tyArg = process.argv[2];
-if (isNaN(tyArg)) {
+let arg = process.argv[2];
+if (isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log('My number is: ' + tyArg);
+  console.log('My number is: ' + arg);
 }
